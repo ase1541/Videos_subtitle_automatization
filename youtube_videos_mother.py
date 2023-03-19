52b3304b3efe1c130704e6852283c6b07004556a
ase1541 <sierralvaro56@gmail.com>
2023-03-19 22:43:12 +0100

    Added extra links and download video

--- youtube_videos_mother.py
+++ youtube_videos_mother.py
@@ -1,84 +1,63 @@
 import os
 import ffmpeg
-
-# Input video file path
-video_file_path = input("Enter video file path: ")
-
-# Input subtitle file path
-subtitle_file_path = input("Enter subtitle file path: ")
-
-# Output folder path
-output_folder = input("Enter output folder path: ")
-
-# Create output folder if it doesn't exist
-if not os.path.exists(output_folder):
-    os.makedirs(output_folder)
-
-# Output video file path
-output_video_file_path = os.path.join(output_folder, f"{os.path.splitext(os.path.basename(video_file_path))[0]}_subtitled.mp4")
-
-# Get video stream info
-probe = ffmpeg.probe(video_file_path)
-video_stream = next((stream for stream in probe['streams'] if stream['codec_type'] == 'video'), None)
-if video_stream is None:
-    print('No video stream found in input file')
-    exit()
-
-# Get subtitle stream info
-subtitle_stream = ffmpeg.input(subtitle_file_path)
-if subtitle_stream.streams.video:
-    print('Subtitle file should not contain any video stream')
-    exit()
-
-# Add subtitles to video stream
-subtitled_stream = ffmpeg.filter([video_stream['index'], subtitle_stream['index']], 'subtitles', force_style='Fontsize=22')
-
-# Create output file
-output = ffmpeg.output(subtitled_stream, f'{output_video_file_path}', vcodec='libx264', crf=18, acodec='copy')
-output.run()
-
-print(f"Subtitled video saved to {output_video_file_path}")
+from pytube import YouTube
+import pytube
+import youtube_transcript_api
+import gspread
+import pandas as pd
+from oauth2client.service_account import ServiceAccountCredentials
+from extra_data import gsheet_credentials_path
+import yt_dlp
 
 
+def extract_links(credential_path=gsheet_credentials_path) -> pd.DataFrame:
+    """
 
+    Args:
+        credential_path: Path to the Gsheet credentials. Remember for next time,
+        you need to create an application, then add drive and gsheets apis,
+        create service account and finally add json key to it.
 
-import os
-import ffmpeg
+    Returns:
+        links_df: Dataframe with only Not Done videos to extract
 
-# Input video file path
-video_file_path = input("Enter video file path: ")
+    """
+    scope = ["https://spreadsheets.google.com/feeds",
+             "https://www.googleapis.com/auth/spreadsheets",
+             "https://www.googleapis.com/auth/drive.file",
+             "https://www.googleapis.com/auth/drive"]
 
-# Input subtitle file path
-subtitle_file_path = input("Enter subtitle file path: ")
+    # Authentication
+    creds = ServiceAccountCredentials.from_json_keyfile_name(credential_path, scope)
+    client = gspread.authorize(creds)
 
-# Output folder path
-output_folder = input("Enter output folder path: ")
+    # Get current sheet records
+    sheet = client.open("Videos_mama")
+    Expenses_Log = sheet.worksheet("Videos Mama")
+    data = Expenses_Log.get_values()
+    links_df = pd.DataFrame(data, columns=["", "", 'Numero',
+                                           'Titulo del video',
+                                           'Nombre sin espacios',
+                                           'Link del video',
+                                           'Status',
+                                           'Comentario',
+                                           'Paid?'])[["Nombre sin espacios", "Link del video", "Status"]]
+    return links_df.loc[links_df['Status'] == 'Not DONE']
 
-# Create output folder if it doesn't exist
-if not os.path.exists(output_folder):
-    os.makedirs(output_folder)
+def download_video(video_url, output_folder):
+    # Create output folder if it doesn't exist
+    if not os.path.exists(output_folder):
+        os.makedirs(output_folder)
 
-# Output video file path
-output_video_file_path = os.path.join(output_folder, f"{os.path.splitext(os.path.basename(video_file_path))[0]}_subtitled.mp4")
+    # Set options for video download
+    ydl_opts = {
+        'format': 'bestvideo[height<=720]+bestaudio/best[height<=720]',
+        'outtmpl': os.path.join(output_folder, '%(title)s.%(ext)s'),
+        'merge_output_format': 'mp4',
+    }
 
-# Get video stream info
-probe = ffmpeg.probe(video_file_path)
-video_stream = next((stream for stream in probe['streams'] if stream['codec_type'] == 'video'), None)
-if video_stream is None:
-    print('No video stream found in input file')
-    exit()
+    # Download video
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([video_url])
 
-# Get subtitle stream info
-subtitle_stream = ffmpeg.input(subtitle_file_path)
-if subtitle_stream.streams.video:
-    print('Subtitle file should not contain any video stream')
-    exit()
-
-# Add subtitles to video stream
-subtitled_stream = ffmpeg.filter([video_stream['index'], subtitle_stream['index']], 'subtitles', force_style='Fontsize=22')
-
-# Create output file
-output = ffmpeg.output(subtitled_stream, f'{output_video_file_path}', vcodec='libx264', crf=18, acodec='copy')
-output.run()
-
-print(f"Subtitled video saved to {output_video_file_path}")
+    print(f"Video saved to {output_folder}")
